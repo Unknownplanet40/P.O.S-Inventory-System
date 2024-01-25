a94a155e7aa2989976455e466c8142ff1c38172c
Ryan James Capadocia <ryanjamesc4@gmail.com>
2024-01-25 15:10:06 +0800

    Update generation time in mls_database.sql

--- Components/Database/mls_database.sql
+++ Components/Database/mls_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 25, 2024 at 07:41 AM
+-- Generation Time: Jan 25, 2024 at 08:08 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
